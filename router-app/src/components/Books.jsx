071990526f160d7322434d2ac6f9b8c09e50b6d5
ravindrapv <ravindra.ravi4849@gmail.com>
2022-11-24 03:43:56 +0530

    added books-data

--- router-app/src/components/Books.jsx
+++ router-app/src/components/Books.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import Data from './BookData.json'
 
 export default function Books() {
     return (
         <>
             <div className="container-2">
-                <h3>Books</h3>
+                <h3 className=' text-center'>Books</h3>
+                <ul className=' flex flex-wrap justify-between gap-1'>
+                    {
+                        Data.map(d => {
+                            return <div className="card">
+                                <li>title:{d.TITLE}</li>
+                                <h2>author:{d.AUTHOR}</h2>
+                                <p>dec:{d.PHYSICAL_DESCRIPTION}</p>
+                            </div>
+                        })
+                    }
+                </ul>
             </div>
         </>
-
     )
 }
